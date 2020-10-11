d0bc59a168a95ba4f4d36c444cb4e31efc330caf
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2020-10-11 19:26:20 +0300

    change db table name

--- src/main/createTable.sql
+++ src/main/createTable.sql
@@ -1,10 +1,10 @@
-create table if not exists proglang(
+create table if not exists language(
      name text UNIQUE,
      description text,
      rating INT UNIQUE
 );
 
-insert into proglang(name, description, rating) values
+insert into language(name, description, rating) values
     ('Java', 'class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible', 5),
     ('C#', 'object-oriented (class-based) programming language developed by Microsoft', 4),
     ('Python', 'interpreted, high-level and general-purpose programming language', 1),
