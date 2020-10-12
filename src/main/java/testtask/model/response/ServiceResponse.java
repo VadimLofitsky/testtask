11b9d6be274be0b3f5e28d2611cb495fc2515074
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2020-10-12 05:16:55 +0300

    add comments

--- src/main/java/testtask/model/response/ServiceResponse.java
+++ src/main/java/testtask/model/response/ServiceResponse.java
@@ -8,17 +8,22 @@ import testtask.model.Language;
 import java.util.List;
 
 
+// class for generating response messages (json)
+
 public class ServiceResponse {
     private static ObjectMapper mapper = new ObjectMapper();
 
+    // returns json containing list of persisted languages
     public static JsonNode get(List<Language> list) {
         return mapper.valueToTree(list);
     }
 
+    // returns json containing the language only
     public static JsonNode get(Language language) {
         return mapper.valueToTree(language);
     }
 
+    // returns json containing both state and language in 'body' property
     public static JsonNode get(ResponseState state, Language body) {
         ObjectNode node = mapper.createObjectNode();
         JsonNode bodyNode = mapper.valueToTree(body);
@@ -29,6 +34,7 @@ public class ServiceResponse {
         return node;
     }
 
+    // returns json containing the state only
     public static JsonNode get(ResponseState state) {
         ObjectNode node = mapper.createObjectNode();
         node.put("state", state.toString());
--- src/main/java/testtask/validation/LanguageValidator.java
+++ src/main/java/testtask/validation/LanguageValidator.java
@@ -8,6 +8,9 @@ import testtask.exception.InvalidLanguageNameException;
 import testtask.exception.InvalidLanguageRatingException;
 import testtask.service.DBService;
 
+
+// class with validation methods for language name and rating
+
 @Component
 @PropertySource("classpath:langValidation.properties")
 public class LanguageValidator {
@@ -15,16 +18,20 @@ public class LanguageValidator {
     @Autowired
     DBService dbService;
 
+    // minimal valid rating value
     @Value("${rating.min}")
     private int RATING_MIN;
 
+    // maximal valid rating
     @Value("${rating.max}")
     private int RATING_MAX;
 
+    // list of valid language names
     @Value("${validNames}")
     private String[] VALID_NAMES;
 
-
+    // checks whether the specified name is in the list of valid names
+    // otherwise throws an exception
     public void validateName(String name) {
         for (String langName : VALID_NAMES) {
             if (name.equals(langName)) {
@@ -35,12 +42,15 @@ public class LanguageValidator {
         throw new InvalidLanguageNameException(name, VALID_NAMES);
     }
 
+    // checks whether the specified rating is inside of valid range
+    // otherwise throws an exception
     public void validateRating(int rating) {
         if (rating < RATING_MIN || rating > RATING_MAX) {
             throw new InvalidLanguageRatingException(rating, RATING_MIN, RATING_MAX);
         }
     }
 
+    // replace absent description with an empty string
     public String validateDescription(String description) {
         return description == null ? "" : description;
     }
